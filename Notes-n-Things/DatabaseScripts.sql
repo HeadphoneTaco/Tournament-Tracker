0bd63aab4d951786ae71e31c66f8184d225e9bc5
MH <michael.hardwicke92@gmail.com>
2023-01-02 14:49:39 -0700

    Move Notes to Separate Folder, add sql scripts file

---
+++ Notes-n-Things/DatabaseScripts.sql
@@ -0,0 +1,19 @@
+-- Creates the Prizes table
+
+USE [Tournaments]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Prizes](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[PlaceNumber] [int] NOT NULL,
+	[PlaceName] [nvarchar](50) NOT NULL,
+	[PrizeAmount] [money] NOT NULL,
+)
+
+
