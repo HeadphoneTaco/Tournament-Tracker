be87222802e68d0c709b699c2b2172cdc4c21633
MH <michael.hardwicke92@gmail.com>
2023-01-04 16:47:14 -0700

    Update Global Config

--- Tournament Tracker/TrackerLibrary/GlobalConfig.cs
+++ Tournament Tracker/TrackerLibrary/GlobalConfig.cs
@@ -10,22 +10,22 @@ namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+        public static IDataConnection Connections { get; private set; }
 
-        public static void InitializeConnections(bool database, bool textFiles)
+        public static void InitializeConnections(DatabaseType db)
         {
-            if (database == true)
+            if (db == DatabaseType.Sql)
             {
                 // TODO - Set up the SQL Connector properly
                 SqlConnector sql = new SqlConnector();
-                Connections.Add(sql);
+                Connections = sql;
             }
 
-            if (textFiles == true)
+            else if (db == DatabaseType.TextFile)
             {
                 // TODO - Create Text Connection
                 TextConnector text = new TextConnector();
-                Connections.Add(text);
+                Connections = text;
             }
         }
 
