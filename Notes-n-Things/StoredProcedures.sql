572c3ec7451a92af2ec78df22e1988acb55110ab
MH <michael.hardwicke92@gmail.com>
2023-01-04 16:12:49 -0700

    Add connections to Sql Connector

--- Notes-n-Things/StoredProcedures.sql
+++ Notes-n-Things/StoredProcedures.sql
@@ -1,3 +1,5 @@
+--
+--
 USE [Tournaments]
 GO
 /****** Object: StoredProcedure [dbo].[spPrizes_GetByTournament] Script Date: 2023-01-02 ******/
@@ -21,3 +23,32 @@ BEGIN
 
 
 END
+--
+--
+USE [Tournaments]
+GO
+/****** Object:  StoredProcedure [dbo].[spPrizes_Insert]    Script Date: 2023-01-04 3:57:49 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[spPrizes_Insert]
+	@PlaceNumber int,
+	@PlaceName nvarchar(50),
+	@PrizeAmount money,
+	@PrizePercentage float,
+	@id int = 0 output
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO dbo.Prizes (PlaceNumber, PlaceName, PrizeAmount, PrizePercentage)
+	VALUES (@PlaceNumber, @PlaceName, @PrizeAmount, @PrizePercentage);
+
+	SELECT @id = SCOPE_IDENTITY();
+
+
+END
+
