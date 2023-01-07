e1f4962a6a0bbfd0383247391b8df2b207aa6c92
MH <michael.hardwicke92@gmail.com>
2023-01-06 17:43:59 -0700

    Add CreatePerson to IDataConnections

--- Tournament Tracker/TrackerLibrary/DataAccess/IDataConnection.cs
+++ Tournament Tracker/TrackerLibrary/DataAccess/IDataConnection.cs
@@ -6,5 +6,6 @@ namespace TrackerLibrary.DataAccess
     public interface IDataConnection
     {
         PrizeModel CreatePrize(PrizeModel model);
+        PersonModel CreatePerson(PersonModel model);
     }
 }
