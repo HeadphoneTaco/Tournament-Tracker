6b24aa22fd84c8378e090224b430632f6c7af32b
MH <michael.hardwicke92@gmail.com>
2023-01-09 12:00:57 -0700

    Update Stored SQL Procedures

--- Notes-n-Things/StoredProcedures.sql
+++ Notes-n-Things/StoredProcedures.sql
@@ -8,7 +8,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER PROCEDURE [dbo].[spPeople_GetAll]
+CREATE PROCEDURE [dbo].[spPeople_GetAll]
 
 AS
 BEGIN
@@ -29,7 +29,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER PROCEDURE [dbo].[spPeople_GetByLastName]
+CREATE PROCEDURE [dbo].[spPeople_GetByLastName]
 
 	@LastName nvarchar(100)
 
@@ -52,7 +52,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER PROCEDURE [dbo].[spPeople_Insert] 
+CREATE PROCEDURE [dbo].[spPeople_Insert] 
 	@FirstName nvarchar(100),
 	@LastName nvarchar(100),
 	@EmailAddress nvarchar(100),
@@ -80,7 +80,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
         -- Gets all the prizes for a given tournament
-ALTER PROCEDURE [dbo].[spPrizes_GetByTournament]
+CREATE PROCEDURE [dbo].[spPrizes_GetByTournament]
     @TournamentId int
 AS
 BEGIN
@@ -104,7 +104,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER PROCEDURE [dbo].[spPrizes_Insert]
+CREATE PROCEDURE [dbo].[spPrizes_Insert]
 	@PlaceNumber int,
 	@PlaceName nvarchar(50),
 	@PrizeAmount money,
@@ -124,3 +124,24 @@ BEGIN
 END
 --
 --
+USE [Tournaments]
+GO
+/****** Object:  StoredProcedure [dbo].[spTeam_GetByTournament]    Script Date: 2023-01-09 12:00:15 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[spTeam_GetByTournament]
+	@TournamentId int
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT t.*
+	FROM dbo.Teams t
+	INNER JOIN dbo.TournamentEntries e on t.id = e.TeamId
+	WHERE e.TournamentId = @TournamentId;
+
+END
