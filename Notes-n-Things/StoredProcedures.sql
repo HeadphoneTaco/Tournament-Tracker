b8bf15dc2cddf49c83436100f0bdb54775d5cb0d
MH <michael.hardwicke92@gmail.com>
2023-01-16 16:07:11 -0700

    More Stored Procedures

--- Notes-n-Things/StoredProcedures.sql
+++ Notes-n-Things/StoredProcedures.sql
@@ -169,3 +169,48 @@ BEGIN
 
 
 END
+--
+--
+USE [Tournaments]
+GO
+/****** Object:  StoredProcedure [dbo].[spTournaments_GetAll]    Script Date: 2023-01-16 3:21:21 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[spTournaments_GetAll]
+
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+    SELECT *
+	FROM dbo.Tournaments
+	WHERE Active = 1;
+
+END
+--
+--
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE dbo.spMatchupEntries_GetByMatchup
+		@MatchupId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    
+	SELECT *
+	FROM MatchupEntries
+	WHERE MatchupId = @MatchupId
+
+END
+GO
